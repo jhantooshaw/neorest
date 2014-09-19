23ac39832b793be0f9b2d1d48893f5f477801e1f
jhantooshaw <jhantoo.shaw@gmail.com>
2014-09-19 20:46:45 +0530

    Working on subdomain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-   $tld_length = 3
+   $tld_length = 1
 end
