7a815c7d4602e758a6a6ecf0181fdaa124969c21
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-15 20:59:15 +0530

    Add deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,12 +27,12 @@ after "deploy", "deploy:restart"
 namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      #from = source.next_revision(current_revision)
+      #if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
+      #else
+      #  logger.info "Skipping asset pre-compilation because there were no asset changes"
+      #end
     end
   end
   
