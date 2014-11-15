5bb3c7d53294561e8cd02def8a848127cb49b9f3
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-15 22:26:13 +0530

    Add deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       #from = source.next_revision(current_revision)
       #if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
       #else
       #  logger.info "Skipping asset pre-compilation because there were no asset changes"
       #end
@@ -38,6 +38,7 @@ namespace :deploy do
   
   desc "Restart passenger app"
   task :restart do
+    run "cp #{shared}/config/server.database.yml #{release_path}/config/database.yml"
     run "ln -s #{shared_path}/assets #{current_path}/public/assets"
     run "touch #{File.join(current_path, 'tmp', 'restart.txt') }"
   end  
