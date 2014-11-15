0bffaeb9c10ff900ee7576e311065a7c875b16cf
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-15 22:37:52 +0530

    Add deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,23 +27,19 @@ after "deploy", "deploy:restart"
 namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      #from = source.next_revision(current_revision)
-      #if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
-      #else
-      #  logger.info "Skipping asset pre-compilation because there were no asset changes"
-      #end
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
     end
   end
   
   desc "Restart passenger app"
-  task :restart do
-    #run "cp #{shared}/config/server.database.yml #{release_path}/config/database.yml"
-    #run "ln -s #{shared_path}/config/server.database.yml #{current_path}/config/database.yml"
-    run "ln -s #{shared_path}/assets #{current_path}/public/assets"
+  task :restart do    
     run "touch #{File.join(current_path, 'tmp', 'restart.txt') }"
   end  
-  
   
 end
 
