27b7b8285a0d2c881cdb72d2d633c36c801fa798
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-17 23:44:45 +0530

    Change in glyphicons fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   
   $tld_length = 0
+  config.assets.paths += %W("#{Rails.root}/vendor/assets/fonts")
 
 end
